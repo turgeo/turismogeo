ed316f645356e677027522e1ec56655e62f5cc74
VirginiaPereiro <vir_pereiro@hotmail.com>
2015-03-04 09:09:52 +0100

    Cambio en el fichero Index

--- www/js/app.js
+++ www/js/app.js
@@ -23,8 +23,8 @@ angular.module('misMapas',
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
+// 'starter.services' is found in servicios.js
+// 'starter.controllers' is found in controladores.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
@@ -46,7 +46,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+  // Each state's controller can be found in controladores.js
   $stateProvider
 
   // setup an abstract state for the tabs directive
